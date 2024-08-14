aeb93f0773616afd1a8943532135905da69f72cd
Rosy581 <jamesheintz10@icloud.com>
2024-08-14 03:51:25 -0400

    :3

--- README.md
+++
@@ -1,4 +0,0 @@
-# Rosy581.github.io
-webshart
-Go website ur in the wrong place
-Https://rosy581.github.io
---
+++ index.js
@@ -0,0 +1,11 @@
+const send = (msg) =>{
+    fetch("https://discord.com/api/webhooks/1273184051171426426/IuWc0ZQz8BG9aTtIroeQj73mFHmz09htuXYLxrZrxApsaST2LHJF5RSB7CvzMWtEc9ft",
+        {
+            method:"POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify({
+                content:msg
+            })
+        }
+    )
+}
