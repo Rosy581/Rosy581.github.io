85e04f73718e0ab3d6ae829d81260877369a6ae8
Rosy581 <jamesheintz10@icloud.com>
2024-08-14 03:57:52 -0400

    g

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const send = (msg) =>{
+const send = () =>{
     fetch("https://discord.com/api/webhooks/1273184051171426426/IuWc0ZQz8BG9aTtIroeQj73mFHmz09htuXYLxrZrxApsaST2LHJF5RSB7CvzMWtEc9ft",
         {
             method:"POST",
             headers: {"Content-Type":"application/json"},
             body: JSON.stringify({
-                content:msg
+                content:document.getElementById("msgBox").value
             })
         }
     )
